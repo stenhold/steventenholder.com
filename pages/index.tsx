21be64817ef32d9082a6fd610ea4f4f2ca101be1
stenhold <doctorduality@gmail.com>
2024-03-01 00:03:41 -0800

    Okay here it is.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+// Define an interface for the ImageLink props
+interface ImageLinkProps {
+  href: string;
+  imgSrc: string;
+  text: string;
+  gridClass?: string; // Make gridClass optional as it might not always be provided
+}
+
+// Use the ImageLinkProps interface for the ImageLink component's props
+const ImageLink: React.FC<ImageLinkProps> = ({ href, imgSrc, text, gridClass = '' }) => (
+  <li className={`flex-none ${gridClass}`}>
+    <Link href={href} passHref>
+      <a>
+        <Image src={imgSrc} alt={text} width={500} height={300} className="rounded-lg shadow-md" layout="responsive" />
+        <span className="mt-2 text-xl font-bold text-white">{text}</span>
+      </a>
+    </Link>
+  </li>
+);
+
 // Header component
 const Header = () => (
   <div className="text-center mt-20">
     <Image src="/steven.png" alt="Steven ten Holder" width={500} height={300} className="rounded-lg shadow-md" layout="intrinsic" />
     <h2 className="text-3xl font-bold text-white mt-4">Hello, I&#39;m Steven ten Holder</h2>
   </div>
-);
-
-// ImageLink component adjusted to receive grid placement classes
-const ImageLink = ({ href, imgSrc, text, gridClass }) => (
-  <li className={`flex-none ${gridClass}`}>
-    <Link href={href} passHref>
-      <Image src={imgSrc} alt={text} width={500} height={300} className="rounded-lg shadow-md" layout="responsive" />
-      <span className="mt-2 text-xl font-bold text-white">{text}</span>
-    </Link>
-  </li>
 );
 
 // Updated ImagePanel component for two-row layout
@@ -36,7 +46,6 @@ const ImagePanel = () => (
         text="Acorn Biolabs"
         gridClass=""
       />
-      {/* Adjust the gridClass if needed to explicitly place items, but in this simple layout, it may not be necessary */}
       <ImageLink
         href="/podcasts"
         imgSrc="/podcast_conversations.jpg"
@@ -53,7 +62,6 @@ const ImagePanel = () => (
   </div>
 );
 
-
 // Footer component
 const Footer = () => (
   <footer className="p-10 mt-4 bg-black w-full text-center text-white">
@@ -64,7 +72,6 @@ const Footer = () => (
 // Adjust the main container of your Home component
 export default function Home() {
   return (
-    // Adjusted classes: Removed 'px-4 m-4' to prevent overflow and applied 'overflow-x-hidden' to hide any accidental horizontal overflow
     <div className="space-y-4 w-full flex flex-col items-center overflow-x-hidden">
       <Header />
       <ImagePanel />
