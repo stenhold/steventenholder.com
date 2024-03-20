e94b977c955926187de5101526956cb7d7098398
Steven ten Holder <steve.tenholder@gmail.com>
2024-03-20 00:30:39 -0700

    pod and blog done 2

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -123,7 +123,6 @@ const HeaderMobile = () => (
                 width="100%"
                 height="500"
                 frameBorder="0"
-                allowTransparency="true"
                 allow="encrypted-media"
                 className="mt-4">
             </iframe>
