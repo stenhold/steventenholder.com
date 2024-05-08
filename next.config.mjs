6fec6d4e8d4c6d30a4c44a9463c45b29cb454d7d
Steven ten Holder <steve.tenholder@gmail.com>
2024-05-08 12:02:18 -0700

    podcast xml update 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Add your other configurations here if necessary
+  };
+  
+  export default nextConfig;
+  
